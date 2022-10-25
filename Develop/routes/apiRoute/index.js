b608ea500d54eb870e2fae3939f3c67bbd95d4a3
Samrebilas <sam.rebilas@gmail.com>
2022-10-25 15:27:11 -0400

    heroku commit

--- Develop/routes/apiRoute/index.js
+++ Develop/routes/apiRoute/index.js
@@ -1,3 +1,20 @@
 const router = require ("express").Router()
-router.get("")
+const notes = require("../../db/db.json");
+const uuid = require("uuid")
+const fs = require("fs")
+const path = require("path")
+router.get("./notes", (req,res)=>{
+    res.json(notes)
+})
+router.post("/notes", (req, res) =>{
+    const notes = require("../../db/db.json")
+    req.bodoy.id = uuid.v4();
+    console.log(req.body)
+    notes.pish(req.body)
+    fs.writeFileSync(
+        path.join(__dirname + "../../../db/db.json"),
+        JSON.stringify(notes)
+    )
+    res.json(req.body)
+})
 module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const htmlRoutes = require("./Develop/routes/htmlRoute");
 const app = express();
+const apiRoutes = require("./Develop/routes/apiRoute")
+const fs = require("fs")
+const path = require("path")
 app.use("/", htmlRoutes);
+app.use("/api", apiRoutes)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 app.get('./Develop/routes/htmlRoute', (req, res) => {
   res.send('Hello!');
 });
